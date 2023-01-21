7feedfedec9ca8b07a61fe73a75446c5c56c4154
Rendi prayata wibawa <54298924+rendiwibawa@users.noreply.github.com>
2023-01-21 15:58:07 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        int menu;
+        do {
+            System.out.println("===== SELAMAT DATANG DI KAIKW ======");
+            System.out.println("1. Cek jadwal kereta");
+            System.out.println("2. Lihat harga tiket");
+            System.out.println("3. Pemesanan tiket");
+            System.out.println("4. Cek status pemesanan");
+            System.out.println("5. Cetak tiket");
+            System.out.println("6. Exit");
+            System.out.println(">> ");
+            menu = scan.nextInt();
+            switch(menu){
+                case 1:
+                
+            }
+        } while (menu != 6);
+
     }
-    //Hallo
+    // Hallo
 }
