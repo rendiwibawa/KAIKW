3028b677b61cdc563dfa6d2391b02b21d230d03c
Rendi prayata wibawa <54298924+rendiwibawa@users.noreply.github.com>
2023-01-21 18:18:26 +0700

    update jadwal

--- src/Jadwal.java
+++ src/Jadwal.java
@@ -1,13 +1,33 @@
+import java.util.ArrayList;
+
 class Jadwal {
     String datang;
     String keberangkatan;
-
     Kereta kereta;
 
+    static ArrayList<Jadwal> arrJadwal = new ArrayList<Jadwal>(); 
+    public static void main(String[] args) {
+        inisialData(arrJadwal);
+        lihatJadwal(arrJadwal);
+    }
 
     Jadwal(String datang, String keberangkatan) {
         this.datang = datang;
         this.keberangkatan = keberangkatan;
+    }
+
+    static void inisialData(ArrayList<Jadwal> arrJadwal) {
+        arrJadwal.add(new Jadwal("senin  -13.00  ","senin  -13.00"));
+        arrJadwal.add(new Jadwal("selasa -13.20  ","selasa -13.20"));
+        arrJadwal.add(new Jadwal("minggu -11.00  ","minggu -11.00"));
+    }
+
+    static void lihatJadwal(ArrayList<Jadwal> arr){
+        for (Jadwal jadwal : arr) {
+            System.out.println("==================================================================");
+            System.out.println( "Kedatangan |\t"+jadwal.datang + "|Keberangkatan |\t" + jadwal.keberangkatan);
+        }
+            System.out.println("==================================================================");
     }
     
 }
--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,100 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        ArrayList<Jadwal> arrJadwal = new ArrayList<Jadwal>(); 
-
-        inisialData(arrJadwal);
         int menu;
+
         do {
-            System.out.println("===== SELAMAT DATANG DI KAIKW ======");
+            clearScreen();
+            System.out.println("       SELAMAT DATANG DI KAIKW ");
+            LocalDate Date = LocalDate.now(); // Create a date object
+            LocalTime Time = LocalTime.now(); // Create a date object
+
+            System.out.println("====================================");
+            System.out.println("| Date Now : " + Date +" | "+ Time.getHour()+":"+Time.getMinute()+ " WIB |");
+            System.out.println("====================================");
             System.out.println("1. Cek jadwal kereta"); // ngeprint
             System.out.println("2. Pemesanan tiket"); // Pesen
             System.out.println("3. Cetak tiket"); 
             System.out.println("4. Exit");
             System.out.print(">> ");
             menu = scan.nextInt();
+
             switch(menu){
                 case 1:
-                lihatJadwal(arrJadwal);
+                    Load_Animate.animate(5);
+                    System.out.println();
+                    Jadwal.main(args);
+                    promptEnterKey();
+
+                    break;
+                case 2:
+                    System.out.println("PESAN TIKET");
+                    promptEnterKey();
+                    break;
+                case 3:
+                    System.out.println("CETAK TIKET");
+                    promptEnterKey();
+                    break;
+
             }
+                 
         } while (menu != 4);
         System.out.println("Thanks :3");
-
+        
         scan.close();
     }
 
-    static void lihatJadwal(ArrayList<Jadwal> arr){
-        // int size = arr.size();
-        for (Jadwal jadwal : arr) {
-            System.out.println("==================================================================");
-            System.out.println( "Kedatangan |\t"+jadwal.datang + "|Keberangkatan |\t" + jadwal.keberangkatan);
-        }
-        System.out.println("==================================================================");
-
-    }
-
-    static void inisialData(ArrayList<Jadwal> arrJadwal) {
-        arrJadwal.add(new Jadwal("senin  -13.00  ","senin  -13.00"));
-        arrJadwal.add(new Jadwal("selasa -13.20  ", "selasa -13.20"));
-        arrJadwal.add(new Jadwal("minggu -11.00  ","minggu -11.00"));
-    }
+    public static void promptEnterKey(){
+        System.out.println("Press \"ENTER\" to continue...");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+     }
+    public static void clearScreen() {  
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+    }  
 }
+
+
+class Load_Animate
+{
+    static byte anime;
+    static void animate(int i)
+    {
+        try
+        {
+            for(int j = 0 ; j<=100 ; j++)
+            {
+                switch(anime)
+                {
+                    case 1:
+                        System.out.print("\r[ \\ ] :" + j + "%");
+                        break;
+                    case 2:
+                        System.out.print("\r[ | ] :" + j + "%");
+                        break;
+                    case 3:
+                        System.out.print("\r[ / ] :" + j + "%");
+                        break;
+                    default:
+                        anime = 0;
+                        System.out.print("\r[ - ] :" + j + "%");
+                }
+                anime++;
+                Thread.sleep(i);
+            }
+            System.out.println(" Success");
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println(e);
+        }
+    } 
+}
+
 
